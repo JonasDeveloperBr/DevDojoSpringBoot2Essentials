722a84c98034bcd8f9c6707742bc9c1e270244c6
JonasDeveloperBr <jonas.cba@gmail.com>
2023-04-22 16:01:02 -0400

    Extra course - Indenting and cleaning comments

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -5,11 +5,9 @@ import academy.devdojo.springboot2.requests.AnimePostRequestBody;
 import academy.devdojo.springboot2.requests.AnimePutRequestBody;
 import academy.devdojo.springboot2.service.AnimeService;
 import academy.devdojo.springboot2.util.DateUtil;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springdoc.api.annotations.ParameterObject;
@@ -30,63 +28,63 @@ import java.util.List;
 @Log4j2
 @RequiredArgsConstructor
 public class AnimeController {
-  private final DateUtil dateUtil;
-  private final AnimeService animeService;
+    private final DateUtil dateUtil;
+    private final AnimeService animeService;
 
-  @GetMapping
-  @Operation(
-          summary = "List all animes paginated",
-          description = "The default size is 20, use the parameter size to change the default value",
-          tags = {"anime"}
-  )
-  public ResponseEntity<Page<Anime>> list(@ParameterObject Pageable pageable) {
-    log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-    return ResponseEntity.ok(animeService.listAll(pageable));
-  }
+    @GetMapping
+    @Operation(
+            summary = "List all animes paginated",
+            description = "The default size is 20, use the parameter size to change the default value",
+            tags = {"anime"}
+    )
+    public ResponseEntity<Page<Anime>> list(@ParameterObject Pageable pageable) {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return ResponseEntity.ok(animeService.listAll(pageable));
+    }
 
-  @GetMapping(path = "/all")
-  public ResponseEntity<List<Anime>> listAll() {
-    log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-    return ResponseEntity.ok(animeService.listAllNonPageable());
-  }
+    @GetMapping(path = "/all")
+    public ResponseEntity<List<Anime>> listAll() {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return ResponseEntity.ok(animeService.listAllNonPageable());
+    }
 
-  @GetMapping(path = "/{id}")
-  public ResponseEntity<Anime> findById(@PathVariable long id) {
-    return ResponseEntity.ok(animeService.findByIdOrThrowBadRequestException(id));
-  }
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id) {
+        return ResponseEntity.ok(animeService.findByIdOrThrowBadRequestException(id));
+    }
 
-  @GetMapping(path = "by-id/{id}")
-  public ResponseEntity<Anime> findByIdAuthenticationPrincipal(
-          @PathVariable long id,
-          @AuthenticationPrincipal UserDetails userDetails) {
-    log.info(userDetails);
-    return ResponseEntity.ok(animeService.findByIdOrThrowBadRequestException(id));
-  }
+    @GetMapping(path = "by-id/{id}")
+    public ResponseEntity<Anime> findByIdAuthenticationPrincipal(
+            @PathVariable long id,
+            @AuthenticationPrincipal UserDetails userDetails) {
+        log.info(userDetails);
+        return ResponseEntity.ok(animeService.findByIdOrThrowBadRequestException(id));
+    }
 
-  @GetMapping(path = "/find")
-  public ResponseEntity<List<Anime>> findByName(@RequestParam String name) {
-    return ResponseEntity.ok(animeService.findByName(name));
-  }
+    @GetMapping(path = "/find")
+    public ResponseEntity<List<Anime>> findByName(@RequestParam String name) {
+        return ResponseEntity.ok(animeService.findByName(name));
+    }
 
-  @PostMapping
-  public ResponseEntity<Anime> save(@RequestBody @Valid AnimePostRequestBody animePostRequestBody) {
-    return new ResponseEntity<>(animeService.save(animePostRequestBody), HttpStatus.CREATED);
-  }
+    @PostMapping
+    public ResponseEntity<Anime> save(@RequestBody @Valid AnimePostRequestBody animePostRequestBody) {
+        return new ResponseEntity<>(animeService.save(animePostRequestBody), HttpStatus.CREATED);
+    }
 
-  @DeleteMapping(path = "/admin/{id}")
-  @ApiResponses(value = {
-                  @ApiResponse(responseCode = "204", description = "Successful Operation"),
-                  @ApiResponse(responseCode = "400", description = "When Anime Doesn't Exist in The Database"),
-          })
-  public ResponseEntity<Void> delete(@PathVariable long id) {
-    animeService.delete(id);
-    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-  }
+    @DeleteMapping(path = "/admin/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Successful Operation"),
+            @ApiResponse(responseCode = "400", description = "When Anime Doesn't Exist in The Database"),
+    })
+    public ResponseEntity<Void> delete(@PathVariable long id) {
+        animeService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
-  @PutMapping
-  public ResponseEntity<Void> replace(@RequestBody AnimePutRequestBody animePutRequestBody) {
-    animeService.replace(animePutRequestBody);
-    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-  }
+    @PutMapping
+    public ResponseEntity<Void> replace(@RequestBody AnimePutRequestBody animePutRequestBody) {
+        animeService.replace(animePutRequestBody);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 }
