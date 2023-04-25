4b5ca52b84a886b1b3da014cc040ca4b4e40a61d
JonasDeveloperBr <jonas.cba@gmail.com>
2023-04-25 16:13:34 -0400

    Refactoring - Swagger documentation

--- src/main/java/academy/devdojo/springboot2/constants/MessageConstants.java
+++ src/main/java/academy/devdojo/springboot2/constants/MessageConstants.java
@@ -5,18 +5,13 @@ public abstract class MessageConstants {
     public static final String URL_BASIC = "http://localhost:8080";
     public static final String URL_V1_ANIMES = "/v1/animes";
     public static final String URL_ADMIN_ID = "/admin/{id}";
+
     public static final String KEY_ID = "{id}";
     public static final String SAVED_ANIME = "saved anime {}";
     public static final String SUCCESSFUL_OPERATION = "Successful Operation";
     public static final String WHEN_ANIME_DOESN_T_EXIST_IN_THE_DATABASE = "When Anime Doesn't Exist in The Database";
-    public static final String HTTP_204 = "204";
-    public static final String HTTP_400 = "400";
     public static final String ANIME_NAME_CANNOT_BE_EMPTY = "The anime name cannot be empty";
     public static final String USER_NAME_CANNOT_BE_EMPTY = "The user's name cannot be empty";
-    public static final String ASDF1 = "The user's name cannot be empty";
-    public static final String ASDF2 = "The user's name cannot be empty";
-    public static final String ASDF3 = "The user's name cannot be empty";
-
     public static final String BAD_REQUEST_EXCEPTION_CHECK_DOCUMENTATION = "Bad Request Exception, Check the Documentation";
     public static final String BAD_REQUEST_EXCEPTION_INVALID_FIELDS = "Bad Request Exception, Invalid Fields";
     public static final String CHECK_THE_FIELD_ERROR = "Check the field(s) error";
@@ -24,6 +19,12 @@ public abstract class MessageConstants {
     public static final String THE_ANIME_ID_CANNOT_BE_EMPTY = "The anime id cannot be empty";
     public static final String THIS_IS_THE_ANIME_S_ID = "This is the Anime's id";
     public static final String ANIME_NOT_FOUND = "Anime not Found";
+    public static final String AUTHORIZATION_FAILED = "Authorization Failed";
 
+    public static final String HTTP_200 = "200";
+    public static final String HTTP_201 = "201";
+    public static final String HTTP_204 = "204";
+    public static final String HTTP_400 = "400";
+    public static final String HTTP_401 = "401";
 
 }
--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -6,8 +6,12 @@ import academy.devdojo.springboot2.requests.AnimePutRequestBody;
 import academy.devdojo.springboot2.service.AnimeService;
 import academy.devdojo.springboot2.util.DateUtil;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springdoc.api.annotations.ParameterObject;
@@ -29,32 +33,57 @@ import static academy.devdojo.springboot2.constants.MessageConstants.*;
 @RequestMapping("/v1/animes")
 @Log4j2
 @RequiredArgsConstructor
+@Tag(name = "Animes", description = "Anime Manipulations")
 public class AnimeController {
     private final DateUtil dateUtil;
     private final AnimeService animeService;
 
     @GetMapping
     @Operation(
-            summary = "List all animes paginated",
+            summary = "Retrieve all Animes paginated",
             description = "The default size is 20, use the parameter size to change the default value",
-            tags = {"anime"}
+            security = {@SecurityRequirement(name = "BasicAuthentication")}
     )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = HTTP_200, description = SUCCESSFUL_OPERATION),
+            @ApiResponse(responseCode = HTTP_401, description = AUTHORIZATION_FAILED)
+    })
     public ResponseEntity<Page<Anime>> list(@ParameterObject Pageable pageable) {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return ResponseEntity.ok(animeService.listAll(pageable));
     }
 
     @GetMapping(path = "/all")
+    @Operation(
+            summary = "Retrieve all Animes",
+            description = "No pagination and no filter",
+            security = {@SecurityRequirement(name = "BasicAuthentication")}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = HTTP_200, description = SUCCESSFUL_OPERATION),
+            @ApiResponse(responseCode = HTTP_401, description = AUTHORIZATION_FAILED)
+    })
     public ResponseEntity<List<Anime>> listAll() {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return ResponseEntity.ok(animeService.listAllNonPageable());
     }
 
     @GetMapping(path = "/{id}")
+    @Operation(
+            summary = "Find Anime by Id",
+            description = "Return an Anime",
+            security = {@SecurityRequirement(name = "BasicAuthentication")}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = HTTP_200, description = SUCCESSFUL_OPERATION),
+            @ApiResponse(responseCode = HTTP_400, description = ANIME_NOT_FOUND),
+            @ApiResponse(responseCode = HTTP_401, description = AUTHORIZATION_FAILED)
+    })
     public ResponseEntity<Anime> findById(@PathVariable long id) {
         return ResponseEntity.ok(animeService.findByIdOrThrowBadRequestException(id));
     }
 
+    //TODO remove this endPoint and implement @AuthenticationPrincipal in all other endPoints
     @GetMapping(path = "by-id/{id}")
     public ResponseEntity<Anime> findByIdAuthenticationPrincipal(
             @PathVariable long id,
@@ -64,19 +93,52 @@ public class AnimeController {
     }
 
     @GetMapping(path = "/find")
+    @Operation(
+            summary = "Find Anime by Name",
+            description = "Retrieve a list of Anime",
+            security = {@SecurityRequirement(name = "BasicAuthentication")}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = HTTP_200, description = SUCCESSFUL_OPERATION),
+            @ApiResponse(responseCode = HTTP_400, description = ANIME_NOT_FOUND),
+            @ApiResponse(responseCode = HTTP_401, description = AUTHORIZATION_FAILED)
+    })
     public ResponseEntity<List<Anime>> findByName(@RequestParam String name) {
+        //TODO create an exception when Anime not found and return Http 400
         return ResponseEntity.ok(animeService.findByName(name));
     }
 
     @PostMapping
+    @Operation(
+            summary = "Create a new Anime",
+            description = "Add new Anime",
+            security = {@SecurityRequirement(name = "BasicAuthentication")}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = HTTP_201,
+                    description = SUCCESSFUL_OPERATION,
+                    content = { @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = AnimePostRequestBody.class))
+                    }),
+            @ApiResponse(responseCode = HTTP_401, description = AUTHORIZATION_FAILED),
+            @ApiResponse(responseCode = HTTP_400, description = BAD_REQUEST_EXCEPTION_INVALID_FIELDS)
+    })
     public ResponseEntity<Anime> save(@RequestBody @Valid AnimePostRequestBody animePostRequestBody) {
         return new ResponseEntity<>(animeService.save(animePostRequestBody), HttpStatus.CREATED);
     }
 
     @DeleteMapping(path = "/admin/{id}")
+    @Operation(
+            summary = "Delete an existing Anime",
+            description = "Delete Anime by Id",
+            security = {@SecurityRequirement(name = "BasicAuthentication")}
+    )
     @ApiResponses(value = {
             @ApiResponse(responseCode = HTTP_204, description = SUCCESSFUL_OPERATION),
             @ApiResponse(responseCode = HTTP_400, description = WHEN_ANIME_DOESN_T_EXIST_IN_THE_DATABASE),
+            @ApiResponse(responseCode = HTTP_401, description = AUTHORIZATION_FAILED)
     })
     public ResponseEntity<Void> delete(@PathVariable long id) {
         animeService.delete(id);
@@ -84,6 +146,16 @@ public class AnimeController {
     }
 
     @PutMapping
+    @Operation(
+            summary = "Update an existing Anime",
+            description = "Update Anime by Id",
+            security = {@SecurityRequirement(name = "BasicAuthentication")}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = HTTP_204, description = SUCCESSFUL_OPERATION),
+            @ApiResponse(responseCode = HTTP_400, description = WHEN_ANIME_DOESN_T_EXIST_IN_THE_DATABASE),
+            @ApiResponse(responseCode = HTTP_401, description = AUTHORIZATION_FAILED)
+    })
     public ResponseEntity<Void> replace(@RequestBody AnimePutRequestBody animePutRequestBody) {
         animeService.replace(animePutRequestBody);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
